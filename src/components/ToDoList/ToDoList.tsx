2d36337defcb4a110b5f30e9d459b879cf5fa97a
denver1981denver <dennik1981@gmail.com>
2025-10-22 15:29:28 +0300

    three_lesson

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,16 +1,43 @@
 import { ToDoListItem } from "./ToDoListitem/ToDoListitem";
-import './ToDoList.scss'
+import "./ToDoList.scss";
+import { ToDo } from "../../models/todo-item";
 
-export const ToDoList = () => {
+export const ToDoList = (props: { todos: ToDo[], updateToDo: Function, deleteToDo: Function }) => {
+  const checkedList = () => {
+    return props.todos
+      .filter((item) => !item.isDone)
+      .map((item, idx) => {
+        return (
+          <ToDoListItem
+            toDoItem={item}
+            key={idx}
+            updateToDo={props.updateToDo}
+            deleteToDo={props.deleteToDo}
+          />
+        );
+      });
+  };
+
+  const unCheckedList = () => {
+    return props.todos
+      .filter((item) => item.isDone)
+      .map((item, idx) => {
+        return (
+          <ToDoListItem
+            toDoItem={item}
+            key={idx}
+            updateToDo={props.updateToDo}
+            deleteToDo={props.deleteToDo}
+          />
+        );
+      });
+  };
+
   return (
     <div className="todo-container">
-      <ul className="todo-list failed">
-        <ToDoListItem />
-      </ul>
-      <ul className="todo-list completed">
-        <ToDoListItem />
-       
-      </ul>
+      <ul className="todo-list failed">{checkedList()}</ul>
+
+      <ul className="todo-list completed">{unCheckedList()}</ul>
     </div>
   );
 };
